21b8d3bebd7bc406ce7efaece7447e77bd9e1c7d
Hrishita119 <hr5050@srmist.edu.in>
2026-03-27 11:03:51 +0530

    UC5 DONE

---
+++ UC5.java
@@ -0,0 +1,18 @@
+public class UC5 {
+    public class HelloApp {
+    public static void main(String[] args) {
+        StringBuilder nameBuilder = new StringBuilder(); 
+        String names = null;
+        for (String name : args){
+            nameBuilder.append(name).append(", ");
+            if (nameBuilder.length() > 0){
+                names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            }
+            else{
+                names = "world";
+            }
+        }
+        System.out.println("Hello, " + names + "!");
+    }
+}
+}
